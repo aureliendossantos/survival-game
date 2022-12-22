48a9325d644252b8b5667b82d339928b48b5b564
Aurélien Dos Santos <31006039+aureliendossantos@users.noreply.github.com>
2022-12-22 18:09:03 +0100

    test of ReactiveButton

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import Image from "next/image"
 import Link from "next/link"
+import ReactiveButton from "reactive-button"
 
 import { useState } from "react"
 import useSWR, { useSWRConfig } from "swr"
@@ -27,6 +28,9 @@ async function createUser(name) {
 export default function LoginPage() {
   const [message, setMessage] = useState()
   const { mutate } = useSWRConfig()
+  const [btn1State, setBtn1State] = useState("idle")
+  const [btn2State, setBtn2State] = useState("idle")
+  const [btn3State, setBtn3State] = useState("idle")
   return (
     <>
       <h1>Choisissez un compte</h1>
@@ -50,29 +54,48 @@ export default function LoginPage() {
         <button type="submit">Créer</button>
       </form>
       <h3>Fonctions de debug</h3>
-      <button
+
+      <ReactiveButton
+        color="dark"
+        buttonState={btn1State}
+        idleText="1. Vider la base de données"
+        loadingText="Chargement"
+        successText="Terminé"
         onClick={async () => {
+          setBtn1State("loading")
           setMessage(await query("/api/setup", "DELETE"))
+          setBtn1State("success")
           mutate("/api/users")
         }}
-      >
-        1. Vider la base de données
-      </button>
-      <button
+      />
+      <br />
+      <ReactiveButton
+        color="dark"
+        buttonState={btn2State}
+        idleText="2. Remplir la base avec les valeurs par défaut"
+        loadingText="Chargement"
+        successText="Terminé"
         onClick={async () => {
+          setBtn2State("loading")
           setMessage(await query("/api/setup", "POST"))
+          setBtn2State("success")
           mutate("/api/users")
         }}
-      >
-        2. Remplir la base avec les valeurs par défaut
-      </button>
-      <button
+      />
+      <br />
+      <ReactiveButton
+        color="dark"
+        buttonState={btn3State}
+        idleText="3. Créer une carte"
+        loadingText="Chargement"
+        successText="Terminé"
         onClick={async () => {
+          setBtn3State("loading")
           setMessage(await query("/api/map", "POST"))
+          setBtn3State("success")
+          mutate("/api/users")
         }}
-      >
-        3. Créer une carte
-      </button>
+      />
     </>
   )
 }
