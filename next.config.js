63f80c72dd637c4476ef49552b274bcbdf4369b8
Aurélien Dos Santos <31006039+aureliendossantos@users.noreply.github.com>
2024-07-11 19:02:51 +0200

    CORS setup

--- next.config.js
+++ next.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Allow all domains
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+    ]
+  },
 }
